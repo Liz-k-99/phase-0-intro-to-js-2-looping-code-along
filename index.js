810bf3d1b58a323d3b44b28afd4d674b9a3db8f4
Elizabeth Kershaw <lizzykershaw99@hotmail.com>
2023-09-22 20:53:16 +1000

    loops

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+for (let age = 20; age < 30; age++){
+    console.log(`You are ${age} old.`);
+};
+const gifts = ['car', 'football', 'T.V','shoes'];
+const wrapGifts = (gifts) => {
+    for (let i = 0; i < gifts.length; i++){
+    console.log(`The ${gifts[i]} is wrapped with a bow on the top!`);
+};
+return gifts;
+}
+wrapGifts(gifts);
+
+let names = [];
+const writeCards = (names, events) => {
+    for (let i = 0; i < names.length; i++){
+       return `Thank you ${names[i]} for the ${events} card!`;
+    }
+    return names.push();
+};
+writeCards(names, events);
+
+const countDown = (count) => {
+    while (count >= 0){
+        console.log(count --);
+    }
+};
+countDown(10);
