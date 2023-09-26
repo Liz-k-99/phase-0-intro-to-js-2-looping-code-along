8973856e4da5e61d8492f1cf9b5daf16ad8f01c2
Elizabeth Kershaw <lizzykershaw99@hotmail.com>
2023-09-26 17:43:52 +1000

    function updated

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 // Code your solutions in this file
-for (let age = 20; age < 30; age++){
+for (let age = 20; age < 30; age++) {
     console.log(`You are ${age} old.`);
 };
-const gifts = ['car', 'football', 'T.V','shoes'];
+const gifts = ['car', 'football', 'T.V', 'shoes'];
 const wrapGifts = (gifts) => {
-    for (let i = 0; i < gifts.length; i++){
-    console.log(`The ${gifts[i]} is wrapped with a bow on the top!`);
-};
-return gifts;
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`The ${gifts[i]} is wrapped with a bow on the top!`);
+    };
+    return gifts;
 }
 wrapGifts(gifts);
 
 let names = [];
 const writeCards = (names, events) => {
-    for (let i = 0; i < names.length; i++){
-       return `Thank you ${names[i]} for the ${events} card!`;
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
     }
-    return names.push();
+    return messages;
 };
-writeCards(names, events);
+//writeCards(names, events);
 
 const countDown = (count) => {
-    while (count >= 0){
-        console.log(count --);
+    while (count >= 0) {
+        console.log(count--);
     }
 };
 countDown(10);
